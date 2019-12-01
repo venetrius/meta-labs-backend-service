7ab4aabb6426c063a06a8ee4a2073b20934047ea
Mike Chui <mikel.k.khui@gmail.com>
2019-11-30 17:28:11 -0700

    Used passport-github2 example with redirect with hardcoded client address. Should replace with CLIENT_URL

--- src/index.js
+++ src/index.js
@@ -7,7 +7,53 @@ const cookieSession = require('cookie-session');
 const morgan        = require("morgan");
 const app           = express();
 const cors          = require('cors');
+const path          = require('path');
+const passport      = require('passport');
+const GithubAuthStr = require('passport-github2').Strategy; 
 require('dotenv').config();
+
+// Add this to your .env from your OAuth App credentials
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+// see example https://github.com/cfsghost/passport-github/blob/master/examples/login/app.js
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete GitHub profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the GitHubStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and GitHub
+//   profile), and invoke a callback with a user object.
+passport.use(new GithubAuthStr({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3030/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      console.log('inpassport.use:', profile);
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -18,12 +64,50 @@ app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 const datahelpers = require('./DataHelpers/data-helpers');
 
 const projectRoutes = require("./routes/projects.js")(datahelpers);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// GET /auth/github
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in GitHub authentication will involve redirecting
+//   the user to github.com.  After authorization, GitHub will redirect the user
+//   back to this application at /auth/github/callback
+app.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/github/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/auth/github' }),
+  function(req, res) {
+    // WIP: Need to send data back
+    res.redirect('http://localhost:3000/projects/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('http://localhost:3000/');
+});
+
 app.use("/projects", projectRoutes);
+
+// create a page to serve up for testing OAuth and redirect
 
 const server = app.listen(process.env.PORT || PORT, () => {
 
